6484bf9ffbdad8535d60c5c88be81124e133a18d
karasu yami <demigodlinty@gmail.com>
2023-06-07 21:02:00 -0500

    made some implements to the web page

---
+++ src/pages/Homepage.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+
+function Homepage() {
+    return (
+        <div>
+            <header className='app-header'><h1>Job application simulator</h1></header>
+            <section id='disclaimer'>
+            <p>welcome to the job application simulator
+               this is a simulater SO DO NOT PUT REAL PERSONAL INFO
+               and most importantly enjoy
+            </p>
+            </section>
+            <footer>
+                <p>socials</p>
+            </footer>
+        </div>
+    );
+}
+
+export default Homepage;

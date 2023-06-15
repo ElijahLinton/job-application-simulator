9376df62ce715857a477f88fbef29a88ac0493c6
karasu yami <demigodlinty@gmail.com>
2023-06-14 19:48:32 -0500

    added a new set of form data so user can submit their answers

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,20 +1,59 @@
-import React, {useState} from 'react';
+import { useState } from "react";
+
 
 function Homepage() {
-    return (
-        <div>
-            <header className='app-header'><h1>Job application simulator</h1></header>
-            <section id='disclaimer'>
-            <p>welcome to the job application simulator
-               this is a simulater SO DO NOT PUT REAL PERSONAL INFO
-               and most importantly enjoy
-            </p>
-            </section>
-            <footer>
-                <p>socials</p>
-            </footer>
-        </div>
-    );
+  const [indexPage, setIndexPage] = useState(1);
+  function handleAnswer(){
+    
+    var answer = new FormData(document.querySelector("form"));
+        console.log("form data: ");
+        for(var pair of answer.entries()){
+          console.log(pair[0]+':' +pair[1]);
+        }
+        return false;
+  }
+
+
+
+  return (
+    <div>
+      <header className="app-header">
+        <h1>Job application simulator</h1>
+      </header>
+      <section className="current">
+        <p>
+          welcome to the job application simulator this is a simulater SO DO NOT
+          PUT REAL PERSONAL INFO and most importantly enjoy
+        </p>
+      </section>
+
+        <section className="inactive">
+            <h1>What position are you interested in?</h1>
+            <form onSubmit={handleAnswer}>
+              
+            <label className="container">web developer
+                <input type='radio' name="radio"/>
+                    <span className='checkmark'></span>
+             </label>
+
+             <label className="container"> Data analyst
+                <input type='radio' name="radio"/>
+                    <span className='checkmark'></span>
+             </label>
+
+             <label className="container"> sales repesenative
+                <input type='radio' name="radio"/>
+                    <span className='checkmark'></span>
+             </label>
+             <input type="submit" name="submit" value='submit'/>
+           </form>
+        </section>
+
+      <footer>
+        <p>socials</p>
+      </footer>
+    </div>
+  );
 }
 
 export default Homepage;
